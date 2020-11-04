821a7396437ba1b3cc94ee2a8a6a778a6c28026d
Cyriaque <cyriaquedelaunay@gmail.com>
2020-11-04 22:13:17 +0100

    added social links

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,6 +4,9 @@ import { Link } from 'react-router-dom';
 import logo from '../logo.png';
 function NavBar() {
   const [t] = useTranslation();
+
+  const twitterLink = encodeURI(`https://twitter.com/intent/tweet?text=${t('commons.title')}\n\nhttps://youtube-cutter.tools`);
+  const facebookLink = encodeURI(`https://www.facebook.com/sharer/sharer.php?u=https://youtube-cutter.tools`);
   return (
     <nav className="navbar">
       <Link to="/" className="navbar-brand">
@@ -19,6 +22,14 @@ function NavBar() {
           </Link>
         </li> */}
       </ul>
+      <div className="navbar-content ml-auto">
+        <a className="btn btn-link btn-lg" href={twitterLink} rel="noopener noreferrer" target="_blank">
+          <i className="fab fa-twitter"></i>
+        </a>
+        <a className="btn btn-link btn-lg" href={facebookLink} rel="noopener noreferrer" target="_blank">
+          <i className="fab fa-facebook"></i>
+        </a>
+      </div>
     </nav>
   );
 }
