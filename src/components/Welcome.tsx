b6f076e831c66aa334e56d94bee30141fc19c875
Cyriaque <cyriaquedelaunay@gmail.com>
2021-01-02 18:37:12 +0100

    added drag and drop support

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,11 +1,37 @@
 import LinkForm from './LinkForm';
-import React from 'react';
+import React, { useCallback } from 'react';
 import '../styles/welcome.scss';
 import Mockup from './Mockup';
+import { useDropzone } from 'react-dropzone';
+import classnames from 'classnames';
+import { useTranslation } from 'react-i18next';
+import useJob from '../hooks/useJob';
 
 function Welcome() {
+  const { t } = useTranslation();
+  const { setFile } = useJob();
+
+  async function handleFileSelected(files: any[]) {
+    if (files && files[0]) {
+      setFile(files[0]);
+    }
+  }
+
+  const onDrop = useCallback((acceptedFiles) => {
+    // Do something with the files
+    handleFileSelected(acceptedFiles);
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
   return (
-    <div className="row container mt-0 mt-md-10 welcome full-height">
+    <div className={classnames('row container mt-0 mt-md-10 welcome full-height', { 'drag-mode': isDragActive })} {...getRootProps()}>
+      <input {...getInputProps()} accept="video/mp4,video/x-m4v,video/*" />
+
+      <div className="drag-overlay">
+        <span>{t('commons.dropFileHere')}</span>
+      </div>
+
       <div className="col">
         <LinkForm />
       </div>
